0d0e0270ceccce93f87bb10e961d90e3e245e888
tushar-454 <imtushar454@gmail.com>
2024-01-24 22:26:22 +0600

    full update task and get specific task using email api added

--- Controller/taskController.js
+++ Controller/taskController.js
@@ -107,10 +107,60 @@ const handleStatusTask = async (req, res, next) => {
   }
 };
 
+/**
+ * update a task
+ * this is a put method update some value
+ * title, description, deadline and priroty
+ */
+const updateTask = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const { title, description, deadline, priroty } = req.body;
+    const result = await Task.findOneAndUpdate(
+      { _id: new Object(id) },
+      {
+        title,
+        description,
+        deadline,
+        priroty,
+      }
+    );
+    res.status(200).json({ message: 'success' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * get some specific task by property value
+ * here based on property we get all trash task or all completed task
+ */
+const specificTask = async (req, res, next) => {
+  try {
+    const email = req.params.email;
+    const { property, value } = req.query;
+    if (property === 'trash') {
+      const result = await Task.find({ trash: !!value, email });
+      res.status(200).json(result);
+      return;
+    }
+    if (property === 'status') {
+      const result = await Task.find({ status: value, email });
+      res.status(200).json(result);
+      return;
+    }
+    res.status(200).json({ message: 'success', data: [] });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   addTask,
   getTask,
   delTask,
   trashTask,
   handleStatusTask,
+  updateTask,
+  specificTask,
 };
--- Routes/task.js
+++ Routes/task.js
@@ -7,5 +7,7 @@ router.get('/get/:email', verifyUser, taskController.getTask);
 router.delete('/remove/:id', verifyUser, taskController.delTask);
 router.patch('/trash/:id', verifyUser, taskController.trashTask);
 router.patch('/status/:id', verifyUser, taskController.handleStatusTask);
+router.put('/update/:id', verifyUser, taskController.updateTask);
+router.get('/getSpecific/:email', verifyUser, taskController.specificTask);
 
 module.exports = router;
