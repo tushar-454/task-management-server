d970a78fc66771748e943d4a38a073bf2d73300d
tushar-454 <imtushar454@gmail.com>
2024-01-24 22:29:52 +0600

    index file all routes base added

--- Routes/index.js
+++ Routes/index.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
+const taskRoutes = require('./task');
+const jwtRoutes = require('./jwt');
+const userRoutes = require('./user');
+
+router.use('/api/v1/task', taskRoutes);
+router.use('/api/v1/jwt', jwtRoutes);
+router.use('/api/v1/user', userRoutes);
 
 module.exports = router;
